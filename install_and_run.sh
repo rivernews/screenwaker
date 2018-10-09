6069d789e45db9af7a6bad191f44d5de8372c365
Shaung Cheng <shaungc@umich.edu>
2018-10-08 22:14:09 -0400

    improve conditional install python

--- install_and_run.sh
+++ install_and_run.sh
@@ -1,14 +1,30 @@
-# pip install --user virtualenv
-# python -m virtualenv venv
-python3 -m venv venv
-if [ -d "venv" ]; then
-    . venv/bin/activate
+is_python_three_installed=$(eval command -v python77)
+
+function activate_install_and_run {
+    . ./venv/bin/activate
     pip install pyobjc-core pyobjc
     pip install Pillow
     pip install pyautogui
-    python screenwaker.py
+
+    if [[ ${is_python_three_installed} = "" ]]; then
+        python screenwaker.py
+    else
+        python3 screenwaker.py
+    fi
     deactivate
+}
+
+if [ -d "venv" ]; then
+    :
 else
-    echo cannot find venv
+    if [[ ${is_python_three_installed} = "" ]]; then
+        echo "INFO: python3 not installed, will try to use python2 and install virtualenv..."
+        pip install --user virtualenv
+        python -m virtualenv venv
+    else
+        echo "INFO: found python3."
+        python3 -m venv venv
+    fi
 fi
 
+activate_install_and_run
